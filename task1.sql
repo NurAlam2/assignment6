94582a778f44bc8e535b1891d41fcc56447b1667
Rakib <developernuralam@gmail.com>
2023-11-06 22:49:32 +0600

    noting to change

---
+++ task1.sql
@@ -0,0 +1,5 @@
+SELECT Customers.customer_id, Customers.name, COUNT(Orders.order_id) AS total_orders
+FROM Customers
+LEFT JOIN Orders ON Customers.customer_id = Orders.customer_id
+GROUP BY Customers.customer_id, Customers.name
+ORDER BY total_orders DESC;
